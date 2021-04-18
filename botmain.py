b194bc3212322457654f7e111979deb154194920
anorprogrammer <77531974+anorprogrammer@users.noreply.github.com>
2021-04-19 01:05:03 +0500

    Update botmain.py

--- botmain.py
+++ botmain.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot('1718923163:AAGVqnCBqWKUjTPMTFtSqyA7K_2v1vhTm3c')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.reply_to(message.chat.id, "Assalomu alaykum \n"+message.from_user.first_name)
+    bot.send_message(message.chat.id, "Assalomu alaykum \n"+message.from_user.first_name)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
